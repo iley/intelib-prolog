b06ad6c0d64e240260f65f4a4b9bff91f609b974
Vasiliy Kulikov <segooon@gmail.com>
2010-01-06 21:15:56 +0300

    Script for handy generating deps.mk.

---
+++ gen_deps_mk.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Run 'gcc -MM' with configurable object file name.
+# It generate dependencies both for object and deps.mk
+#
+
+while true;
+do
+    if [ "$1" = '--cxxflags' ]; then
+        shift
+        CXXFLAGS=$1
+        shift
+    elif [ "$1" = '--deps-mk' ]; then
+        shift
+        DEPSMK=$1
+        shift
+    elif [ "$1" = '--files' ]; then
+        shift
+        CPPFILES=$1
+        shift
+    elif [ "$1" = '--prefix' ]; then
+        shift
+        PREFIX=$1
+        shift
+    elif [ "$1" = '--cxx' ]; then
+        shift
+        CXX=$1
+        shift
+    else
+        break
+    fi
+done
+
+for FILE in $CPPFILES
+do
+    OBJECT=`echo $FILE | sed 's/\.[^.]*$/.o/'`
+    $CXX $CXXFLAGS -MM -MT $PREFIX$OBJECT $FILE >> $DEPSMK
+    $CXX $CXXFLAGS -MM -MT $DEPSMK $FILE >> $DEPSMK
+done 
