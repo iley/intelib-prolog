0b583e0a6899f3592db3f597f238fb5a444d01aa
qwerta <qwerta@live.ru>
2010-01-08 23:25:44 +0300

    added functions for process execution

--- win_port/win_port.cpp
+++ win_port/win_port.cpp
@@ -1,6 +1,19 @@
 #include "win_port.hpp"
+
+#ifdef _WIN32
+
 #include <windows.h>
 #include <tlhelp32.h>
+
+#else
+
+#include <unistd.h>
+#include <sys/wait.h>
+
+#endif
+
+#include <stdio.h>
+using namespace std;
 
 pid_t getppid()
 {
@@ -19,4 +32,71 @@ pid_t getppid()
 	}
     CloseHandle(h);
 	return -1;
+}
+
+int execute_cmd_vp(const char *file, const char *argv[])
+{
+#ifdef _WIN32
+// Windows-like process execution
+
+	// First - create a cmd line (concatenation of all args)
+	char *cmd;
+	int cmd_length = 0;
+	for (int i = 0; argv[i]; i++)
+		cmd_length += strlen(argv[i] + 1);
+	
+	cmd = new char[cmd_length];
+	char *p = cmd;
+	for (const char **arg = argv; *arg; arg++)
+	{
+		const char *g = *arg;
+		while (*g) *p++ = *g++;
+		*p++ = ' ';
+	}
+	*(p-1) = '\0';
+
+	STARTUPINFO startupInfo = {0};
+	startupInfo.cb = sizeof(startupInfo);
+	PROCESS_INFORMATION pInfo;
+	
+	BOOL res = CreateProcess(file, cmd, NULL, NULL, FALSE,
+		NORMAL_PRIORITY_CLASS, NULL, NULL, &startupInfo, &pInfo);
+	
+	if (res)
+	{
+		// Waiting for termination
+		res = WaitForSingleObject(pInfo.hProcess, INFINITE) == WAIT_OBJECT_0;
+		CloseHandle(pInfo.hProcess);
+	}
+	
+	delete []cmd;
+	if (res == 0) return -1;
+	
+	return 0;
+	
+#else
+// UNIX-like process execution
+
+	pid_t child_pid = fork();
+	int status;
+	
+	if (child_pid == -1) return -1;
+	else if (child_pid == 0)
+	{
+		execvp(file, argv);
+		return -1;
+	}
+	else
+	{
+		wait(&status);
+		if ( !(WIFEXITED(status) != 0 && WEXITSTATUS(status) == 0) ) return -1;
+	}
+	return 0;
+#endif
+}
+
+int execute_cmd_lp(const char *file, const char *arg0, ...)
+{
+	int res = execute_cmd_vp(file, &arg0);
+	return res;
 }
--- win_port/win_port.hpp
+++ win_port/win_port.hpp
@@ -11,4 +11,7 @@ pid_t getppid();
 
 #endif
 
+int execute_cmd_lp(const char *file, const char *arg0, ...);
+int execute_cmd_vp(const char *file, const char *argv[]);
+
 #endif
