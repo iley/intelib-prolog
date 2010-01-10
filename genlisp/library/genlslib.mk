592abf0b184c3ddbdd970c71796285288fa4581b
Andrey Vikt. Stolyarov <avst at intelib org>
2010-01-10 18:25:26 +0300

    added copyright notice to the files */library/*lib.mk

--- genlisp/library/genlslib.mk
+++ genlisp/library/genlslib.mk
@@ -1,3 +1,16 @@
+#   InteLib                                    http://www.intelib.org
+#   The file genlisp/library/genlslib.mk
+# 
+#   Copyright (c) Andrey Vikt. Stolyarov, 2000-2009
+# 
+# 
+#   This is free software, licensed under GNU GPL v.2
+#   See the file COPYING for further details.
+# 
+#   THERE IS NO WARRANTY OF ANY KIND, EXPRESSED, IMPLIED OR WHATEVER!
+#   Please see the file WARRANTY for the detailed explanation.
+
+
 SHELL = /bin/sh
 
 GLSP_PREFIX=$(TARGETDIRFP)/glsp_
