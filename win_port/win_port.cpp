aa6b166864983dd32b0e6a2fb17a468fdeb4c584
Vasiliy Kulikov <segooon@gmail.com>
2010-01-20 17:38:17 +0300

    Small code style fixes.

--- win_port/win_port.cpp
+++ win_port/win_port.cpp
@@ -26,8 +26,9 @@
 
 #include <stdarg.h>
 
+
 #ifdef _WIN32
-// Not necessary for *nix
+
 pid_t getppid()
 {
     int pid = GetCurrentProcessId();
@@ -39,7 +40,8 @@ pid_t getppid()
     {
         do
         {
-            if (pe.th32ProcessID == pid) return pe.th32ParentProcessID;
+            if (pe.th32ProcessID == pid)
+                return pe.th32ParentProcessID;
         }
         while(Process32Next(h, &pe));
     }
@@ -49,23 +51,24 @@ pid_t getppid()
 
 #endif
 
+
+#ifdef _WIN32
+
 int execute_cmd_vp(const char *file, char * const argv[])
 {
-#ifdef _WIN32
-// Windows-like process execution
-
     // First - create a cmd line (concatenation of all args)
     char *cmd;
     int cmd_length = 0;
     for (int i = 0; argv[i]; i++)
         cmd_length += strlen(argv[i] + 1);
-    
+
     cmd = new char[cmd_length];
     char *p = cmd;
     for (char * const *arg = argv; *arg; arg++)
     {
         const char *g = *arg;
-        while (*g) *p++ = *g++;
+        while (*g)
+            *p++ = *g++;
         *p++ = ' ';
     }
     *(p-1) = '\0';
@@ -73,7 +76,7 @@ int execute_cmd_vp(const char *file, char * const argv[])
     STARTUPINFO startupInfo = {0};
     startupInfo.cb = sizeof(startupInfo);
     PROCESS_INFORMATION pInfo;
-    
+
     BOOL res = CreateProcess(file, cmd, NULL, NULL, FALSE,
         NORMAL_PRIORITY_CLASS, NULL, NULL, &startupInfo, &pInfo);
 
@@ -83,19 +86,25 @@ int execute_cmd_vp(const char *file, char * const argv[])
         res = WaitForSingleObject(pInfo.hProcess, INFINITE) == WAIT_OBJECT_0;
         CloseHandle(pInfo.hProcess);
     }
-    
-    delete []cmd;
-    if (res == 0) return -1;
-    
-    return 0;
-    
-#else
-// UNIX-like process execution
 
+    delete []cmd;
+    if (res == 0)
+        return -1;
+
+    return 0;
+}
+
+#else /* ifdef _WIN32 */
+
+int execute_cmd_vp(const char *file, char * const argv[])
+{
     pid_t child_pid = fork();
     int status;
-    
-    if (child_pid == -1) return -1;
+
+    if (child_pid == -1)
+    {
+        return -1;
+    }
     else if (child_pid == 0)
     {
         execvp(file, argv);
@@ -104,28 +113,31 @@ int execute_cmd_vp(const char *file, char * const argv[])
     else
     {
         wait(&status);
-        if ( !(WIFEXITED(status) != 0 && WEXITSTATUS(status) == 0) ) return -1;
+        if ( !(WIFEXITED(status) != 0 && WEXITSTATUS(status) == 0) )
+            return -1;
     }
     return 0;
-#endif
 }
+
+#endif
 
 int execute_cmd_lp(const char *file, char * const arg0, ...)
 {
     va_list vl;
     int argc = 0;
-    
+
     va_start(vl, arg0);
-    while (va_arg(vl, char*)) argc++;
+    while (va_arg(vl, char*))
+        argc++;
     va_end(vl);
-    
+
     char **argv = new char*[argc + 1];
 
     va_start(vl, arg0);
     for (int i = 0; i < argc; i++)
         argv[i] = va_arg(vl, char*);
     va_end(vl);
-    
+
     int res = execute_cmd_vp(file, argv);
     delete []argv;
     return res;
