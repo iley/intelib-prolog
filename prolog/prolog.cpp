3eda8f731331a9225fdccc5ca32120e11af4d1e7
Ilya Strukov <iley@iley.ru>
2011-09-25 19:34:08 +0400

    prolog: declare some generic stuff

--- prolog/prolog.cpp
+++ prolog/prolog.cpp
@@ -1,1 +1,7 @@
 #include "prolog.hpp"
+
+IntelibX_not_a_prolog_term::
+IntelibX_not_a_prolog_term(SReference a_param) 
+    : IntelibX("Not a prolog term", a_param) {}
+
+IntelibTypeId PlgTerm::TypeId(&SExpression::TypeId, true);
--- prolog/prolog.hpp
+++ prolog/prolog.hpp
@@ -1,4 +1,32 @@
 #ifndef INTELIB_PROLOG_HPP_SENTRY
 #define INTELIB_PROLOG_HPP_SENTRY
 
+#include "../sexpress/sexpress.hpp"
+#include "../sexpress/gensref.hpp"
+#include "../sexpress/custmem.hpp"
+
+class IntelibX_not_a_prolog_term : public IntelibX
+{
+public:
+    IntelibX_not_a_prolog_term(SReference a_param);
+};
+
+class PlgTerm;
+typedef GenericSReference<PlgTerm, IntelibX_not_a_prolog_term> PlgTermRef;
+
+class PlgTermReference : public PlgTermRef {
+public:
+    PlgTermReference() {}
+    PlgTermReference(const PlgTermReference& ref) : PlgTermRef(ref) {}
+    PlgTermReference(const SReference& ref) : PlgTermRef(ref) {}
+    PlgTermReference(SExpression* p) : PlgTermRef(p) {}
+
+    ~PlgTermReference() {}
+};
+
+class PlgTerm : public SExpression {
+public:
+    static IntelibTypeId TypeId;
+};
+
 #endif //INTELIB_PROLOG_HPP_SENTRY
