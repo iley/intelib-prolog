1de707b1054f7d481852cc5fc4bed4becf7e4bf3
Ilya Strukov <iley@iley.ru>
2011-09-28 23:44:57 +0400

    prolog: remove old tests and prepare for the new ones

--- tests/prolog/t_data.cpp
+++ tests/prolog/t_data.cpp
@@ -10,9 +10,6 @@
 //   THERE IS NO WARRANTY OF ANY KIND, EXPRESSED, IMPLIED OR WHATEVER!
 //   Please see the file WARRANTY for the detailed explanation.
 
-
-
-
 #include <stdio.h>
 #include "tests.hpp"
 
@@ -35,45 +32,7 @@ int main()
         poc();
         TestSection("PrologData");
         {
-            PlgSymbol john("john");
-            PlgSymbol george("george");
-            PlgSymbol ann("ann");
-            PlgSymbol brian("brian");
-            PlgSymbol parent("parent");
-
-            parent(john, george) <<= true;
-            parent(john, ann) <<= true;
-            parent(ann, brian) <<= true;
-
-            parent(1,2,3,4) <<= true;
-
-            TESTTR("facts", parent->GetProcedure(2),
-                            "((parent(john, george)) "
-                             "(parent(john, ann)) "
-                             "(parent(ann, brian)))");
-
-            TESTTR("different_arity", parent->GetProcedure(4),
-                                     "((parent(1, 2, 3, 4)))");
-
-            TESTB("undefined_arity0", !parent->GetProcedure(0).GetPtr());
-            TESTB("undefined_arity1", !parent->GetProcedure(1).GetPtr());
-            TESTB("undefined_arity3", !parent->GetProcedure(3).GetPtr());
-            TESTB("undefined_arity500", !parent->GetProcedure(500).GetPtr());
-
-
-            PlgSymbol relative("relative");
-            PlgVariable X("X");
-            PlgVariable Y("Y");
-            PlgVariable Z("Z");
-
-            relative(X, Y) <<= parent(X, Y);
-            relative(X, Y) <<= parent(Y, X);
-            relative(X, Y) <<= relative(X, Z), relative(Z, Y);
-
-            TESTTR("longer_clauses", relative->GetProcedure(2),
-                   "((relative(X, Y) parent(X, Y)) "
-                    "(relative(X, Y) parent(Y, X)) "
-                    "(relative(X, Y) relative(X, Z) relative(Z, Y)))");
+			TESTB("fake", true);
         }
         TestScore();
         poc();
