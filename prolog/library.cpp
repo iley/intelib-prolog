2489f818d57b2d0120b773511eb2eafe5b999422
Ilya Strukov <iley@iley.ru>
2011-12-21 12:41:02 +0400

    prolog: remove junk code and make ";" and "," simple predicates

--- prolog/library.cpp
+++ prolog/library.cpp
@@ -3,67 +3,23 @@
 
 static SListConstructor S;
 
-// Disjunction
-
-IntelibTypeId PlgExpressionDisjunction::TypeId(&PlgExpressionTerm::TypeId, false);
-
-PlgAtom PlgExpressionDisjunction::Atom("<OR>");
-
-PlgReference PlgExpressionDisjunction::RenameVars(const PlgReference &self, NameGeneratorFunction nameGenerator, SHashTable &existingVars) const {
-    SReference newList = *PTheEmptyList;
-    for (SReference p = args; !p.IsEmptyList(); p = p.Cdr()) {
-        PlgReference pref = p.Car();
-        newList.AddAnotherItemToList(pref.RenameVars(nameGenerator, existingVars));
-    }
-    return PlgDisjunction(newList);
-}
-
-bool PlgExpressionDisjunction::Solve(const PlgReference &self, PlgExpressionContinuation &continuation) const {
-    //TODO
-    throw IntelibX_not_implemented();
-}
-
-#if INTELIB_TEXT_REPRESENTATIONS == 1
-SString PlgExpressionDisjunction::TextRepresentation() const {
-    return Join("; ", args);
-}
-#endif
-
-PlgDisjunction operator | (const PlgReference &left, const PlgReference &right) {
-    return PlgDisjunction((S| left, right ));
-}
-
 // Conjunction
 
-IntelibTypeId PlgExpressionConjunction::TypeId(&PlgExpressionTerm::TypeId, false);
-
-PlgAtom PlgExpressionConjunction::Atom("<OR>");
-
-bool PlgExpressionConjunction::Solve(const PlgReference &self, PlgExpressionContinuation &continuation) const {
-    //TODO
+bool PredicateConj(const SReference &args, PlgExpressionContinuation &cont) {
     throw IntelibX_not_implemented();
 }
 
-PlgReference PlgExpressionConjunction::RenameVars(const PlgReference &self, NameGeneratorFunction nameGenerator, SHashTable &existingVars) const {
-    SReference newList = *PTheEmptyList;
-    for (SReference p = args; !p.IsEmptyList(); p = p.Cdr()) {
-        PlgReference pref = p.Car();
-        newList.AddAnotherItemToList(pref.RenameVars(nameGenerator, existingVars));
-    }
-    return PlgConjunction(newList);
+PlgAtom PlgAtomConj(";", 2, PredicateConj);
+
+// Disjunction
+
+bool PredicateDisj(const SReference &args, PlgExpressionContinuation &cont) {
+    throw IntelibX_not_implemented();
 }
 
-#if INTELIB_TEXT_REPRESENTATIONS == 1
-SString PlgExpressionConjunction::TextRepresentation() const {
-    return Join(", ", args);
-}
-#endif
+PlgAtom PlgAtomDisj(";", 2, PredicateDisj);
 
-PlgConjunction operator & (const PlgReference &left, const PlgReference &right) {
-    return PlgConjunction((S| left, right ));
-}
-
-// "==" predicate
+// "=" predicate
 
 bool PredicateEquals(const SReference &args, PlgExpressionContinuation &cont) {
     PlgReference left = args.Car();
