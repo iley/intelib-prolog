ef33efc4f2465cfd26bb7092af5a76ca33fd82ab
Ilya Strukov <iley@iley.ru>
2012-04-07 11:49:37 +0400

    prolog: change "prolog" to "swipl" in build scripts

--- prolog/library/generate.sh
+++ prolog/library/generate.sh
@@ -4,5 +4,5 @@ MODULES="grammar output"
 
 for MODULE in $MODULES
 do
-	prolog -q -s pl/trans.pro -t "translate_stdlib('pl/$MODULE.pro')"
+	swipl -q -s pl/trans.pro -t "translate_stdlib('pl/$MODULE.pro')"
 done
