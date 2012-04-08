5754baa5ef22186695bd25599eb34be84f0e4617
Ilya Strukov <iley@iley.ru>
2012-04-08 11:35:02 +0400

    prolog example: simplify example.cpp and add .clang_complete

--- samples/prolog/cpp/example.cpp
+++ samples/prolog/cpp/example.cpp
@@ -6,23 +6,24 @@ int main() {
     using namespace PlgStdLib;
 
     PlgAtom man("man"), woman("woman"), human("human"), son("son"), parent("parent");
-    PlgAtom bob("bob"), mary("mary"), john("john");
+    PlgAtom bob("bob"), mary("mary"), john("john"), jack("jack");
     PlgVariable X("X"), Y("Y");
 
     PlgDatabase db;
-    db.Add(
-        son(X, Y) <<= parent(Y, X) & man(X),
-        woman(mary),
-        man(bob),
-        man(john),
-        parent(bob, mary),
-        parent(bob, john)
-    );
+    db.Add( son(X, Y) <<= parent(Y, X) & man(X) );
+    db.Add( woman(mary) );
+    db.Add( man(bob) );
+    db.Add( man(john) );
+    db.Add( man(jack) );
+    db.Add( parent(bob, mary) );
+    db.Add( parent(bob, john) );
+    db.Add( parent(bob, jack) );
 
-    PlgAtom print_sons("print_sons");
-    db.Add( print_sons <<= son(X, bob) & write(X) & nl & fail );
-
-    db.Once(print_sons);
+    PlgContinuation cont = db.Query(son(X, bob));
+    while (cont->Next()) {
+        PlgReference result = cont->GetValue(X);
+        printf("%s\n", result->TextRepresentation().c_str());
+    }
 
     return 0;
 }
