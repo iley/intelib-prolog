6d0a830aade39b8b8daf4f26845665619dfb20c0
Ilya Strukov <iley@iley.ru>
2012-04-12 13:42:45 +0400

    prolog: rewrite qsort example

--- samples/prolog/cpp/qsort.cpp
+++ samples/prolog/cpp/qsort.cpp
@@ -1,36 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 #include <prolog/prolog.hpp>
-#include <prolog/utils.hpp>
 
 int main() {
     using namespace PlgStdLib;
 
     SListConstructor S;
-    SReference &Nil = *PTheEmptyList;
+    SReference &nil = *PTheEmptyList;
 
     PlgAtom qsort("qsort"), split("split");
     PlgVariable H("H"), T("T"), R("R"), L("L"), X("X"), Res("Res"), LS("LS"), RS("RS");
 
-    qsort(Nil, Nil) <<= truth;
+    *qsort(nil, nil);
     qsort(H^T, Res) <<= split(H, T, L, R) & qsort(L, LS) & qsort(R, RS) & append(LS, H ^ RS, Res);
 
-    split(X, Nil, Nil, Nil) <<= truth;
+    *split(X, nil, nil, nil);
     split(X, H^T, H^LS, RS) <<= (H <= X) & split(X, T, LS, RS);
     split(X, H^T, LS, H^RS) <<= (H >  X) & split(X, T, LS, RS);
 
-    srand(time(0));
-
-    SReference list = Nil;
-    for (int i = 0; i < 1000; ++i)
-        list = SReference(rand()).MakeCons(list);
-    //printf("Sorting %s...\n", Dump(list));
+    SReference list = (S|3, 1, 2, 5, 4);
 
     PlgContinuation cont = qsort(list, X).Query();
     if (cont->Next()) {
-        //printf("Result: %s\n", Dump(cont->GetValue(X)));
-        printf("Done\n");
+        SReference sortedList = cont->GetValue(X);
+        puts(sortedList->TextRepresentation().c_str());
+    } else {
+        puts("No solution found!");
     }
 
     return 0;
